dd3f265a3c0a763280473a39e08d5a0a0fb95a3d
Maxens Gagne <mgagne@z3r4p6.42lyon.fr>
2024-10-01 18:50:17 +0200

    all results out

--- ex02/inc/PmergeMe.hpp
+++ ex02/inc/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: mgagne <mgagne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 15:21:22 by mgagne            #+#    #+#             */
-/*   Updated: 2024/10/01 12:51:13 by mgagne           ###   ########.fr       */
+/*   Updated: 2024/10/01 18:38:45 by mgagne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 	#include <deque>
 	#include <vector>
 	#include <limits.h>
+	#include <sys/time.h>
 
 
 	void parseInput(int argc, char *argv[]);
@@ -25,7 +26,9 @@
 	std::vector<long> sortVector(std::vector<long> d);
 	std::deque<long> sortDeque(std::deque<long> d, int argc);
 	std::vector<std::pair<long, long> > fillVectorPair(std::vector<long> v);
-	std::vector<long> vSort(std::vector<long> sorted, std::vector<std::pair<long, long> > vPair);
-	std::vector<long> insertElements(std::vector<long> sorted, std::vector<std::pair<long, long> > vPair);
 
+	std::vector<std::pair<long, long> > makePair(std::vector<std::pair<long, long> > vPair);
+
+	std::vector<long> vSort(std::vector<long> sorted, std::vector<std::pair<long, long> > vPair);
+	std::vector<long> insertElements(std::vector<long> sorted, std::vector<std::pair<long, long> > vPair, std::vector<std::pair<long, long> > vPair2);
 #endif
--- ex02/srcs/PmergeMe.cpp
+++ ex02/srcs/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: mgagne <mgagne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 14:23:56 by mgagne            #+#    #+#             */
-/*   Updated: 2024/10/01 14:42:44 by mgagne           ###   ########.fr       */
+/*   Updated: 2024/10/01 18:49:07 by mgagne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,15 +45,28 @@ void mergeInsert(int argc, char *argv[])
 	std::vector<long> v;
 	std::deque<long> d;
 
+	struct timeval start_t, end_t;
+	gettimeofday(&start_t, NULL);
+	gettimeofday(&end_t, NULL);
+
 	for (int i = 1; i < argc; i++)
 	{
 		v.push_back(strtol(argv[i], NULL, 10));
 		d.push_back(strtol(argv[i], NULL, 10));
 	}
+
 	printVector(v);
+	gettimeofday(&start_t, NULL);
 	v = sortVector(v);
-	// printVector(v);
-	// d = sortDeque(d, argc);
+	gettimeofday(&end_t, NULL);
+	std::cout << std::endl;
+	printVector(v);
+	std::cout << "Time for std::vector : " << (end_t.tv_usec - start_t.tv_usec) << " us" << std::endl;
+
+	// gettimeofday(&start_t, NULL);
+	// d = sortDeque(d);
+	// gettimeofday(&end_t, NULL);
+	// std::cout << "Time for std::deque : " << (end_t.tv_usec - start_t.tv_usec) << " us" << std::endl;
 }
 
 std::vector<long> sortVector(std::vector<long> v)
@@ -80,9 +93,27 @@ std::vector<std::pair<long, long> > fillVectorPair(std::vector<long> v)
 	return (vPair);
 }
 
+std::vector<std::pair<long, long> > makePair(std::vector<std::pair<long, long> > vPair)
+{
+	std::vector<std::pair<long, long> > vPair2;
+
+	if (vPair.size() >= 2)
+	{
+		for (size_t i = 0; i < vPair.size(); i++)
+		{
+			if ((i % 2 == 0 || i == 0) && i + 1 < vPair.size())
+			{
+				if (vPair[i].second != -1 && vPair[i].first < vPair[i].second)
+					std::swap(vPair[i].first, vPair[i].second);
+				vPair2.push_back(std::make_pair(vPair[i].first, vPair[i + 1].first));
+			}
+		}
+	}
+	return (vPair2);
+}
+
 std::vector<long> vSort(std::vector<long> sorted, std::vector<std::pair<long, long> > vPair)
 {
-
 	for (size_t i = 0; i < vPair.size(); i++)
 	{
 		if (vPair[i].second != -1 && vPair[i].first < vPair[i].second)
@@ -90,33 +121,36 @@ std::vector<long> vSort(std::vector<long> sorted, std::vector<std::pair<long, lo
 	}
 
 	std::vector<std::pair<long, long> > vPair2;
+	vPair2 = makePair(vPair);
 
-	if (vPair.size() >= 2)
-	{
-		std::cout << std::endl;
-		for (size_t i = 0; i < vPair.size(); i++)
-		{
-			if ((i % 2 == 0 || i == 0) && i + 1 < vPair.size())
-			{
-				std::cout << "FIRST : " << vPair[i].first << " SECOND : " << vPair[i + 1].first << std::endl;
-				vPair2.push_back(std::make_pair(vPair[i].first, vPair[i + 1].first));
-			}
-		}
-	}
 	if (vPair2.size() >= 2)
 		sorted = vSort(sorted, vPair2);
-	if (sorted.size() == 0 && vPair.size() == 1)
-		sorted = insertElements(sorted, vPair);
-	// sorted = insertElements(sorted, vPair);
+
+	if (vPair2.size() == 1)
+	{
+		if (vPair2[0].second != -1 && vPair[0].first < vPair[0].second)
+			std::swap(vPair2[0].first, vPair2[0].second);
+		sorted.push_back(vPair2[0].second);
+		sorted.push_back(vPair2[0].first);
+	}
+	if (vPair2.size() >= 1)
+		sorted = insertElements(sorted, vPair, vPair2);
+
 	return (sorted);
 }
 
-std::vector<long> insertElements(std::vector<long> sorted, std::vector<std::pair<long, long> > vPair)
+std::vector<long> insertElements(std::vector<long> sorted, std::vector<std::pair<long, long> > vPair, std::vector<std::pair<long, long> > vPair2)
 {
-	std::cout << std::endl;
 	for (size_t i = 0; i < vPair.size(); i++)
 	{
-		//jacobsthal - dichotomie
+		if (vPair[i].second != -1)
+			sorted.push_back(vPair[i].second);
+	}
+	std::cout << "vPair 2 size : " << vPair2.size() << std::endl;
+	std::cout << "vPair size : " << vPair.size() << std::endl;
+	if (vPair2.size() * 2 != vPair.size())
+	{
+		sorted.push_back(vPair[vPair.size() - 1].first);
 	}
 	return (sorted);
 }
